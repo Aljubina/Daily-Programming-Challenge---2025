be9e0fe2cf59e2255d3f4ffffeaa756ab8d60812
Aljubina_G <aljubinagavit@gmail.com>
2025-08-20 22:02:17 +0530

    Find All Subarrays with Zero Sum
    You are given an integer array arr of size n. Your task is to find all the subarrays whose elements sum up to zero. A subarray is defined as a contiguous part of the array, and you must return the starting and ending indices of each subarray.
    
    Input:
    An integer array arr of size n where n represents the number of elements in the array.
    Example : 
    Input: [1, 2, -3, 3, -1, 2]
    
    Output:
    Return a list of tuples, where each tuple contains two integers. The starting index (0-based) of the subarray. The ending index (0-based) of the subarray.
    The output should list all subarrays that sum to zero. If no such subarrays are found, return an empty list.
    Example
    Output: [(0, 2), (1, 3)]

---
+++ day6.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<pair<int, int>> findZeroSumSubarrays(vector<int>& arr) {
+    int n = arr.size();
+    vector<pair<int, int>> result;
+    long long prefixSum = 0;
+
+    unordered_map<long long, vector<int>> sumMap;
+
+    for (int i = 0; i < n; i++) {
+        prefixSum += arr[i];
+
+        // Case 1: prefix sum itself is 0 -> subarray (0..i)
+        if (prefixSum == 0) {
+            result.push_back({0, i});
+        }
+
+        if (sumMap.find(prefixSum) != sumMap.end()) {
+            for (int startIdx : sumMap[prefixSum]) {
+                result.push_back({startIdx + 1, i});
+            }
+        }
+
+
+        sumMap[prefixSum].push_back(i);
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> arr = {1, 2, -3, 3, -1, 2};
+
+    vector<pair<int, int>> subarrays = findZeroSumSubarrays(arr);
+
+    cout << "[";
+    for (int i = 0; i < subarrays.size(); i++) {
+        cout << "(" << subarrays[i].first << ", " << subarrays[i].second << ")";
+        if (i != subarrays.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
